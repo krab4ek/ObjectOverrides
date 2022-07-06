2fc0812deaab606cba657d6bf3a579ebfedc5e30
Covali Andrei <krab4ek@gmail.com>
2022-07-06 22:22:44 +0300

    Add project files.

---
+++ ObjectOverrides/Program.cs
@@ -0,0 +1,8 @@
+﻿using ObjectOverrides;
+
+Console.WriteLine("***** Fun with System.Object *****\n");
+Person p1 = new Person();
+
+Console.WriteLine($"ToString: {p1.ToString()}");
+Console.WriteLine($"Hash code: {p1.GetHashCode()}");
+Console.WriteLine($"Type: {p1.GetType()}");
